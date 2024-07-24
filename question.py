a10ba5efc9b85f5332dfe598b15c65d421a30592
Jahicollab <skjahimunnisa@gmail.com>
2024-07-24 17:52:55 +0530

    day2

---
+++ question.py
@@ -0,0 +1,16 @@
+my_list=input()
+for i in range(len(my_list)):
+    print(my_list[i],end=" ")
+print("\n ------------")
+for i in my_list:
+    print(i,end=" ")
+
+
+print("\n")
+
+t="Hello Jahi"
+for i in range(len(t)):
+    print(t[i],end=" ")
+print('\n')
+for i in t:
+    print(i,end=" ")
---
+++ question_8.py
@@ -0,0 +1,13 @@
+ #this is for even indexed elements avg we should add these lines
+
+my_list=list(map(int,input().split(" ")))
+count=0
+sum=0
+avg=0
+for i in range(len(my_list)):
+      #if(i+=2==0):              
+        #print(my_list[i])         
+        count+=1
+        sum+=my_list[i]
+        avg=sum/count
+print(f"{avg} is average of all even numbers in the given list")
